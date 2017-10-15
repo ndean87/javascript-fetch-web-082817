4283a7f4f31619d214ad4459fe7032fc0fa388ec
ndean87 <ndean87@gmail.com>
2017-10-15 13:27:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much.";
+
+// fetch('https://api.github.com/user/repos')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+
+const token = '0efaaea0cb5c863eb4bd3cf19ffc760f1cdd3de8';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
